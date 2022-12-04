aaccd07f494d17a2f840bffb52ee2f076b3ecf53
ndd2102 <ndd.nguyenducdung@gmail.com>
2022-12-04 18:00:54 +0700

    updateChat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -10,7 +10,7 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
     props.channel.id
   }&token=${localStorage.getItem("token")}`;
   const [messageHistory, setMessageHistory] = useState([]);
-  const [mess, setMess] = useState<string>();
+  const [mess, setMess] = useState<string>("");
   const [otherAva, setOtherAva] = useState<Profile[]>([]);
   const didUnmount = useRef(false);
   const ws = new WebSocket(socketUrl);
@@ -47,15 +47,22 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
   }, []);
   setTimeout(() => {
     let objDiv: any = document.getElementById("chatArea");
-
     objDiv.scrollTop = objDiv.scrollHeight;
+    let input = document.getElementById("myInput");
+    input?.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("myBtn")?.click();
+      }
+    });
   }, 0);
   const handleClickSendMessage = useCallback(() => {
     if (mess !== undefined && mess !== "") {
-      sendJsonMessage({ content: `${mess}` });
+      sendJsonMessage({ content: `${mess.trim()}` });
     }
     setMess("");
   }, [mess, sendJsonMessage]);
+
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
     [ReadyState.OPEN]: "Open",
@@ -65,80 +72,117 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
   }[readyState];
 
   return (
-    <form>
-      <span className="inline-flex justify-center items-center ml-4 text-lg">
-        <Avatar
-          img={props.channel.avatar}
-          rounded={true}
-          status="online"
-          statusPosition="bottom-right"
-        />
-      </span>
-      <span className="p-4 text-lg tracking-wide truncate">
-        {props.channel.name}
-      </span>
-      <div>The WebSocket is currently {connectionStatus}</div>
+    <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <span className="absolute text-green-500 right-0 bottom-0">
+              <svg width="20" height="20">
+                <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
+              </svg>
+            </span>
+            <img
+              src={props.channel.avatar}
+              alt=""
+              className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"
+            ></img>
+          </div>
+          <div className="flex flex-col leading-tight">
+            <div className="text-2xl mt-1 flex items-center">
+              <span className="text-gray-700 mr-3 tracking-wide truncate">
+                {props.channel.name}
+              </span>
+            </div>
+            <span className="text-lg text-gray-600 tracking-wide truncate">
+              The WebSocket is currently {connectionStatus}
+            </span>
+          </div>
+        </div>
+      </div>
       <div
         id="chatArea"
         className="h-[600px] flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
       >
         {messageHistory.map(ms)}
       </div>
-      <TextInput id="text" onChange={handleChange}></TextInput>
-      <Button
-        type="reset"
-        onClick={handleClickSendMessage}
-        // disabled={readyState !== ReadyState.OPEN}
-      >
-        Click Me to send
-      </Button>
-    </form>
+      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+        <div className="relative flex">
+          <input
+            id="myInput"
+            type="text"
+            placeholder="Write your message!"
+            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3"
+            onChange={handleChange}
+            value={mess}
+          ></input>
+          <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            <button
+              id="myBtn"
+              type="button"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              onClick={handleClickSendMessage}
+            >
+              <span className="font-bold">Send</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-6 w-6 ml-2 transform rotate-90"
+              >
+                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 
   function ms(value: any, idx: any) {
     const avatar = otherAva.find((obj) => {
       return obj.id === JSON.parse(value.data).senderId;
     });
-    if (JSON.parse(value.data).senderId !== props.profile.userId) {
-      return (
-        <div key={idx} className="chat-message">
-          <div className="flex items-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-              <div className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
-                {[JSON.parse(value.data).content]}
-              </div>
+    if (JSON.parse(value.data).timestamp !== "-1") {
+      if (JSON.parse(value.data).senderId !== props.profile.userId) {
+        return (
+          <div key={idx} className="chat-message">
+            <div className="flex items-end">
+              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                <div className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
+                  {[JSON.parse(value.data).content]}
+                </div>
 
-              <img
-                src={avatar?.avatar}
-                alt="ava"
-                className="w-6 h-6 rounded-full order-1"
-                onClick={() => console.log(avatar?.name)}
-              ></img>
-            </div>
-          </div>
-        </div>
-      );
-    } else
-      return (
-        <div key={idx} className="chat-message">
-          <div className="flex items-end justify-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-              <div className="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white">
-                {[JSON.parse(value.data).content]}
+                <img
+                  src={avatar?.avatar}
+                  alt="ava"
+                  className="w-6 h-6 rounded-full order-1"
+                  onClick={() => console.log(avatar?.name)}
+                ></img>
               </div>
-              <img
-                src={avatar?.avatar}
-                alt="ava"
-                className="w-6 h-6 rounded-full order-2"
-              ></img>
             </div>
           </div>
-        </div>
-      );
+        );
+      } else
+        return (
+          <div key={idx} className="chat-message">
+            <div className="flex items-end justify-end">
+              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+                <div className="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white">
+                  {[JSON.parse(value.data).content]}
+                </div>
+                <img
+                  src={avatar?.avatar}
+                  alt="ava"
+                  className="w-6 h-6 rounded-full order-2"
+                ></img>
+              </div>
+            </div>
+          </div>
+        );
+    }
   }
-
   function handleChange(event: { target: { value: any } }) {
-    setMess(event.target.value.trim());
+    setMess(event.target.value);
   }
 };
 export default Chat;
