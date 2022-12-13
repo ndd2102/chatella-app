a5ab2fffd3c368d09f2d614a4181416f5b41f2c7
Khanh Linh <khanhlinh.ph2002@gmail.com>
2022-12-13 15:12:38 +0700

    fix UI chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -7,6 +7,7 @@ import { Profile } from "../../types/profile";
 import { Channel } from "../../types/channel";
 import EmojiPicker from "emoji-picker-react";
 import { addMinutes, format } from "date-fns";
+import AvatarGroupCounter from "flowbite-react/lib/esm/components/Avatar/AvatarGroupCounter";
 
 const Chat = (props: { profile: Profile; channel: Channel }) => {
   const socketUrl = `ws://w42g11.int3306.freeddns.org//channel/chat?channelId=${
@@ -89,9 +90,9 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
   }[readyState];
 
   return (
-    <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
-      <div className="sm:items-center justify-between py-3 border-b-2 border-gray-200">
-        <div className="mb-5">
+    <div className="flex-1 justify-between flex flex-col h-screen">
+      <div className="sm:items-center mt-4 justify-between py-3 border-b border-gray-200">
+        <div className="mb-5 ml-4">
           <span className="text-xl font-bold">Member ({otherAva.length})</span>
           <span
             className="float-right pr-6 underline text-slate-500 hover:cursor-pointer hover:text-slate-600"
@@ -101,16 +102,24 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
           </span>
         </div>
         <React.Fragment>
-          <Avatar.Group>
-            {otherAva.map((value, id) => (
-              <Avatar
-                key={id}
-                img={value.avatar}
-                rounded={true}
-                stacked={true}
-              />
-            ))}
-          </Avatar.Group>
+          <div className="ml-6">
+            <Avatar.Group>
+              {otherAva.map((value, index) => {
+                if (index > 7) {
+                  return <AvatarGroupCounter total={otherAva.length - index} />;
+                } else {
+                  return (
+                    <Avatar
+                      key={index}
+                      img={value.avatar}
+                      rounded={true}
+                      stacked={true}
+                    />
+                  );
+                }
+              })}
+            </Avatar.Group>
+          </div>
           <Modal
             show={show}
             size="md"
@@ -123,11 +132,11 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
                 <h3 className="text-xl font-medium mb-8 text-gray-900 dark:text-white">
                   All members
                 </h3>
-                {otherAva.map((userId, id) => (
-                  <div key={id} className="space-y-2 mt-2">
+                {otherAva.map((userId, index) => (
+                  <div key={index} className="space-y-2 mt-2">
                     <Label className="inline-flex justify-center items-center text-lg ">
                       <Avatar img={userId.avatar} />
-                      <div className="ml-4">{userId.name}</div>
+                      <div className="ml-6">{userId.name}</div>
                     </Label>
                   </div>
                 ))}
@@ -153,16 +162,16 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
       </div>
       <div
         id="chatArea"
-        className=" flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+        className=" flex flex-col space-y-4 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
       >
         {messageHistory.slice(1).map(check)}
       </div>
-      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="border-t-2 border-gray-200 p-4">
         <div className="relative flex">
           <textarea
             id="myInput"
             placeholder="Write your message!"
-            className="w-11/12 focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 bg-gray-200 rounded-full "
+            className="w-11/12 focus:outline-none resize-none pr-16 h-12 focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 bg-gray-200 rounded-full "
             onChange={handleChange}
             value={mess}
           ></textarea>
@@ -177,7 +186,7 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
             )}
             <button
               type="button"
-              onClick={handlShowEmoji}
+              onClick={handleShowEmoji}
               className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
             >
               <svg
@@ -200,7 +209,7 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
             <button
               id="myBtn"
               type="button"
-              className=" rounded-full p-2  duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              className=" rounded-full p-2 h-12 text-white bg-blue-500 hover:bg-blue-600 focus:outline-none"
               onClick={handleClickSendMessage}
             >
               <svg
@@ -217,7 +226,7 @@ const Chat = (props: { profile: Profile; channel: Channel }) => {
       </div>
     </div>
   );
-  function handlShowEmoji() {
+  function handleShowEmoji() {
     setShowEmoji(!showEmoji);
   }
 
